a9b0c6d70a6b7f7106615993ec83d0e8ce2ca832
28ralph <lee.ralph@sbcglobal.net>
2023-11-04 09:23:32 -0500

    dom manipulation

---
+++ script.js
@@ -0,0 +1,70 @@
+let quotes = [
+  `I live my life a quarter mile at a time`,
+  `I said a ten-second car, not a ten-minute car`,
+  `You can have any brew you want... as long as it's a Corona.`,
+  `You almost had me? You never had me - you never had your car!`,
+  `I don't have friends. I have family.`,
+  `It don't matter if you win by an inch or a mile. Winning's winning.`
+];
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  // Random quote of the day generator
+  const randomQuote = function () {
+    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+  };
+  randomQuote();
+
+  // Do all of your work inside the document.addEventListener  
+
+  // Part 1
+  document.querySelector("#main-title").innerHTML = "Hello World!";
+  // Part 2
+  document.querySelector("body").style.backgroundColor = "yellow";
+
+
+  // Part 3
+  const favorite = document.querySelector("#favorite-things");
+  favorite.removeChild(favorite.lastElementChild);
+
+  // Part 4
+  const special = document.querySelectorAll(".special-title");
+
+  for (let i = 0; i < special.length; i++) {
+    special[i].style.fontSize = "2rem";
+  }
+
+
+  // Part 5
+  const races = document.querySelector("#past-races");
+  races.removeChild(races.children[3])
+
+  // Part 6
+  const race = document.createElement("li");
+  race.textContent = "San Francisco";
+  races.append(race);
+
+  // Part 7
+  const elements = document.createElement("div");
+  document.body.append(elements)
+  elements.classList.add("blog-post")
+  const h2 = document.createElement("h2");
+  h2.innerHTML = "What's up?";
+  elements.append(h2);
+
+  const p = document.createElement("p");
+  document.body.append(p);
+  p.textContent = "Hey you";
+  elements.append(p)
+  // Part 8
+  const title = document.querySelector("#quote-title");
+  title.addEventListener("click", randomQuote)
+
+  // Part 9
+  const all = document.querySelectorAll(".blog-post");
+  for (let i = 0; i <all.length; i++){
+    
+  }
+
+
+
+});
